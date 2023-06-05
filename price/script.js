3bd5d37824a0f8ca49b397b44cce72a0ab600ac4
David <David07994415>
2023-06-05 22:34:34 +0800

    draft

---
+++ price/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $(".navbar-btn").click(function () {
+      $(".navbar-collapse").toggleClass("show");
+    })
+});
+
+// const swiper = new Swiper('.swiper', {
+//     slidesPerView:1,
+//     spaceBetween:0,
+//     breakpoints: {
+//         376: {
+//           slidesPerView: 3,
+//           spaceBetween: 24,
+//           slidesPerGroup: 3,
+//         },
+//     },
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable:true,
+//      },
+// });
+
+
+// 開起選單
+$('.dropdown-btn').click(function(e) {
+  $('.dropdown-menu').toggleClass('show');
+});
+
+// 切換按鈕文字
+$('.new-to-old').click(function(e) {
+  e.preventDefault();
+  $('.dropdown-menu').toggleClass('show');
+  $('.dropdown-btnText').text($('.new-to-old').text());
+});
+
+$('.old-to-new').click(function(e) {
+  e.preventDefault();
+  $('.dropdown-menu').toggleClass('show');
+  $('.dropdown-btnText').text($('.old-to-new').text());
+});
+
+
+$('.qa-item').click(function(e) {
+  $(this).toggleClass('active');
+  $(this).find('.add-icon').toggleClass('d-none');
+  $(this).find('.remove-icon').toggleClass('d-none');
+  $(this).find('.collapse-content p').toggleClass('show');
+});
